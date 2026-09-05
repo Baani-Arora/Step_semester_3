b7db19d28cad59b72f949d2c682c06646c575b37
Baani Arora <your_github_email@example.com>
2026-09-05 07:04:09 +0530

    Add Week 3 OOP homework assignment

---
+++ src/main/java/oop/assignment_problems/EmployeeStatic.java
@@ -0,0 +1,29 @@
+package oop.assigment_problems;
+
+class EmployeeStatic {
+
+    String empName;
+    double salary;
+
+    static String companyName = "Bright Horizon Technologies";
+    static int employeeCount = 0;
+
+    EmployeeStatic(String empName, double salary) {
+        this.empName = empName;
+        this.salary = salary;
+        employeeCount++;
+    }
+
+    static void printCompanyInfo() {
+        System.out.println(companyName);
+        System.out.println("Employees on record: " + employeeCount);
+    }
+
+    public static void main(String[] args) {
+        new EmployeeStatic("A", 10000);
+        new EmployeeStatic("B", 20000);
+        new EmployeeStatic("C", 30000);
+
+        EmployeeStatic.printCompanyInfo();
+    }
+}
