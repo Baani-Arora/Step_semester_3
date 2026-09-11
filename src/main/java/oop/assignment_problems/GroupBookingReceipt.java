644c8fff003a0567c3100eeed30cb8655818fa46
Baani Arora <your_github_email@example.com>
2026-09-12 02:49:52 +0530

    Complete Week 5 movie booking problems

---
+++ src/main/java/oop/assignment_problems/GroupBookingReceipt.java
@@ -0,0 +1,18 @@
+package oop.assignment_problems;
+
+class GroupBookingReceipt extends BookingReceipt {
+
+    private int groupSize;
+
+    public GroupBookingReceipt(String bookingId,
+                               String[] seatNumbers,
+                               int groupSize) {
+
+        super(bookingId, seatNumbers);
+        this.groupSize = groupSize;
+    }
+
+    public int getGroupSize() {
+        return groupSize;
+    }
+}
