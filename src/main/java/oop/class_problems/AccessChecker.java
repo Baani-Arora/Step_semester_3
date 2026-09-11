6ca49d7e838bdae41fba7a3435744a6ab05b6dde
Baani Arora <your_github_email@example.com>
2026-09-12 02:52:41 +0530

    Add Week 5 class problems

---
+++ src/main/java/oop/class_problems/AccessChecker.java
@@ -0,0 +1,73 @@
+package oop.class_problems;
+
+class MovieTicket {
+
+    private String seatNumber;
+    String screenId;
+    protected double ticketPrice;
+    public String movieTitle;
+}
+
+class AccessChecker {
+
+    static String classifyAccess(String fieldModifier, String accessorContext) {
+
+        switch (fieldModifier) {
+
+            case "private":
+                return accessorContext.equals("SAME_CLASS") ? "ALLOWED" : "DENIED";
+
+            case "default":
+                return (accessorContext.equals("SAME_CLASS")
+                        || accessorContext.equals("SAME_PACKAGE"))
+                        ? "ALLOWED" : "DENIED";
+
+            case "protected":
+                switch (accessorContext) {
+
+                    case "SAME_CLASS":
+                    case "SAME_PACKAGE":
+                    case "SUBCLASS_DIFFERENT_PACKAGE_OWN_TYPE":
+                        return "ALLOWED";
+
+                    default:
+                        return "DENIED";
+                }
+
+            case "public":
+                return "ALLOWED";
+        }
+
+        return "DENIED";
+    }
+
+    static String summarizeBatch(String[][] attempts) {
+
+        int allowed = 0;
+        int denied = 0;
+
+        for (String[] attempt : attempts) {
+
+            if (classifyAccess(attempt[0], attempt[1]).equals("ALLOWED"))
+                allowed++;
+            else
+                denied++;
+        }
+
+        return "Allowed: " + allowed + " | Denied: " + denied;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(classifyAccess("private", "SAME_CLASS"));
+        System.out.println(classifyAccess("protected", "DIFFERENT_PACKAGE"));
+
+        String[][] sample = {
+                {"default", "SAME_PACKAGE"},
+                {"default", "DIFFERENT_PACKAGE"},
+                {"public", "DIFFERENT_PACKAGE"}
+        };
+
+        System.out.println(summarizeBatch(sample));
+    }
+}
